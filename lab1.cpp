ff79b0bff4b4157411d5bacecadf11885977c6b4
Jacob Halvorson <jhalvorson6687@icloud.com>
2020-01-20 19:18:11 -0800

    Init

---
+++ lab1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include "md5.h"
+
+using namespace std;
+
+int main() {
+	string HASH = "I1JjSUzgQ5rPId/BCVAiJ.";
+	string pass = "aaaaaa";
+	string salt = "4fTgjp6q";
+	sting magic = "$1$";
+	bool match = false;
+
+	while(password != "zzzzz"
+
+}
